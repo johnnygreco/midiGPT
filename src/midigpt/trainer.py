a17765a667b66c1d9d4d3b48e1ac30884d2c2222
johnnygreco <jgreco.ai@gmail.com>
2023-03-07 22:20:08 -0500

    change validation eval method

--- src/midigpt/trainer.py
+++ src/midigpt/trainer.py
@@ -1,8 +1,7 @@
 import warnings
-from typing import Dict
 
 import torch
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from tqdm.rich import tqdm
 from tqdm.std import TqdmExperimentalWarning
 
@@ -20,44 +19,16 @@ class Trainer:
     def __init__(self, config: TrainConfigure):
         self.config = config
         self.model = GPT(config)
+        self.loss_history = []
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=config.learning_rate)
         self.device = utils.get_auto_device() if config.device == "auto" else config.device
         self.model.to(self.device)
 
-    @torch.no_grad()
-    def estimate_loss(self) -> Dict[str, float]:
-        mean_losses = {}
-        self.model.eval()
-        for split in ["train", "valid"]:
-            loader_iter = iter(getattr(self, f"{split}_loader"))
-            losses = torch.zeros(self.config.eval_iters)
-            for i in range(self.config.eval_iters):
-                batch = next(loader_iter)
-                x, y = [t.to(self.device) for t in batch]
-                losses[i] = self.model(x, y)[1].item()
-            mean_losses[split] = losses.mean()
-        self.model.train()
-        return mean_losses
-
-    def _print_current_loss(self, batch_num: int, epoch: int):
-        if batch_num % self.config.eval_interval == 0:
-            losses = self.estimate_loss()
-            tqdm.write(f"Epoch {epoch} | train loss: {losses['train']:.4f}, valid loss: {losses['valid']:.4f}")
-
-    def train(self, dataset: DatasetType, train_fraction: float = 0.9, shuffle: bool = True):
-        indices = torch.arange(len(dataset))
-        self.train_loader = DataLoader(
-            Subset(dataset, indices[: int(len(dataset) * train_fraction)]),
-            batch_size=self.config.batch_size,
-            shuffle=shuffle,
-        )
-        self.valid_loader = DataLoader(
-            Subset(dataset, indices[int(len(dataset) * train_fraction) :]),
-            batch_size=self.config.batch_size,
-            shuffle=shuffle,
-        )
+    def train(self, dataset: DatasetType, shuffle: bool = True):
+        self.train_loader = DataLoader(dataset=dataset, batch_size=self.config.batch_size, shuffle=shuffle)
         batches_per_epoch = self.config.batches_per_epoch if self.config.batches_per_epoch else len(self.train_loader)
-        with tqdm(total=self.config.num_epochs * batches_per_epoch, desc="Training") as progress_bar:
+        with tqdm(total=self.config.num_epochs * batches_per_epoch, desc="Training:") as progress_bar:
+            running_loss = 0.0
             for epoch in range(1, self.config.num_epochs + 1):
                 for batch_num, (x, y) in enumerate(self.train_loader, start=1):
                     x, y = x.to(self.device), y.to(self.device)
@@ -65,7 +36,12 @@ class Trainer:
                     self.optimizer.zero_grad(set_to_none=True)
                     self.loss.backward()
                     self.optimizer.step()
+                    running_loss += self.loss.item()
+                    self.loss_history.append(self.loss.item())
+                    if batch_num % self.config.eval_interval == 0:
+                        average_loss = running_loss / self.config.eval_interval
+                        tqdm.write(f"epoch: {epoch:<4.0f}  |  batch: {batch_num:<7.0f}  |  loss: {average_loss:<.4f}")
+                        running_loss = 0.0
                     progress_bar.update(1)
-                    self._print_current_loss(batch_num, epoch)
                     if self.config.batches_per_epoch and batch_num > self.config.batches_per_epoch:
                         break
