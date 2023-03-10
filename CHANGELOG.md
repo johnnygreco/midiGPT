80c399a4a8d68f9f87ed672cba9e9945b6cf1cf7
semantic-release <semantic-release>
2023-03-10 17:45:31 -0500

    0.0.1-beta.1
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.0.1-beta.1 (2023-03-10)
+
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if os.path.isfile("VERSION"):
     with open("VERSION") as version:
         __version__ = version.read().strip("v")
 else:
-    __version__ = "0.0.0"
+    __version__ = "0.0.1-beta.1"
 
 with open("README.md", "r") as fh:
     long_description = fh.read() or ""
@@ -23,4 +23,15 @@ setup(
     package_dir={"": "src"},
     python_requires=">=3.8",
     install_requires=install_requires,
+    url="https://github.com/johnnygreco/midiGPT",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
