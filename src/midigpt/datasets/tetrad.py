b1f082bae668372217c25ab59d2e671d951a33f9
johnnygreco <jgreco.ai@gmail.com>
2023-04-15 20:17:34 +0000

    add bach chorale encoder

--- src/midigpt/datasets/tetrad.py
+++ src/midigpt/datasets/tetrad.py
@@ -31,23 +31,20 @@ class BachChoraleDataset(Dataset):
     _C1 = 36
     _A5 = 81
 
-    def __init__(self, chorales, context_length, shift_c1_to_one=True):
+    def __init__(self, chorales, context_length, c1_encoded=1):
         assert len(chorales[0][0]) == 4, "chorales must be a list of lists of 4 integers"
         assert context_length % 4 == 0, "context_length must be a multiple of 4"
-        shift = self._C1 - 1 if shift_c1_to_one else 0
+        self.encoder = BachChoralesEncoder(c1_encoded=c1_encoded)
         corpus = torch.tensor(list(chain.from_iterable(chain.from_iterable(chorales))), dtype=torch.long)
-        corpus[corpus > 0] = corpus[corpus > 0] - shift
-
+        corpus = self.encoder.encode(corpus)
         self.corpus = corpus.tolist()
-        self.min_note = self._C1 - shift
-        self.max_note = self._A5 - shift
-        assert (
-            min(set(self.corpus) - {0}) >= self.min_note and max(self.corpus) <= self.max_note
-        ), "input notes must be in the original data format -> 0 + [36, 81]"
-        self.vocab = [0] + (torch.arange(self._C1, self._A5 + 1) - shift).tolist()
+        self.shift = self._C1 - c1_encoded
+        self.min_note = self._C1 - self.shift
+        self.max_note = self._A5 - self.shift
+        self.vocab = [0] + (torch.arange(self._C1, self._A5 + 1) - self.shift).tolist()
         self.vocab_size = len(self.vocab)
         self.context_length = context_length
-        self.shift_c1_to_one = shift_c1_to_one
+        assert min(set(self.corpus) - {0}) >= self.min_note and max(self.corpus) <= self.max_note, "invalid note range"
 
     def __len__(self):
         return len(self.corpus) - self.context_length
